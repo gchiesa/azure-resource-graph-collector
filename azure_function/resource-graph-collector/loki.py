5f23e89762824ae80cfc63f5dc09b0cf6a5905a0
Giuseppe Chiesa <giuseppe.chiesa@tomtom.com>
2022-03-29 10:18:56 +0200

    refactored loki published to only use emitter

--- azure_function/resource-graph-collector/loki.py
+++ azure_function/resource-graph-collector/loki.py
@@ -3,48 +3,22 @@ import logging
 from copy import deepcopy
 from typing import Tuple, Optional, List
 
-from logging_loki import LokiHandler
+from logging_loki.emitter import LokiEmitterV1
 
 MAX_LABELS = 10
-
-
-class LokiCustomHandler(LokiHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(
-            url=kwargs.get('url', None),
-            tags=kwargs.get('tags', None),
-            auth=kwargs.get('auth', None),
-            version=kwargs.get('version', None),
-        )
-        self._logger_for_errors = kwargs.get('logger_for_errors', None)
-
-    def emit(self, record: logging.LogRecord):
-        """Send log record to Loki."""
-        try:
-            self.emitter(record, self.format(record))
-        except Exception as e:
-            self._logger_for_errors.error(f"Error while publishing on Loki instance. Type: {str(type(e))}, "
-                                          f"Value: {str(e)}")
-            self.emitter.close()
-            raise
 
 
 class LokiPublisher(object):
     def __init__(self, loki_endpoint: str, auth: Tuple[str, str], tags: Optional[dict] = None):
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.publisher = None
+        self.emitter = None
         self.endpoint = loki_endpoint
         self.auth = auth
         self.tags = tags or {}
         self._initialize()
 
     def _initialize(self):
-        self.publisher = logging.getLogger(f"{self.__class__.__name__}-publisher")
-        for handler in self.publisher.handlers:
-            self.publisher.removeHandler(handler)
-
-        self.publisher.addHandler(LokiCustomHandler(url=self.endpoint, tags=self.tags, auth=self.auth, version="1",
-                                                    logger_for_errors=self.logger))
+        self.emitter = LokiEmitterV1(self.endpoint, self.tags, self.auth)
 
     def _prepare_tags(self, data: dict, fields_to_labels: Optional[List] = None):
         """loki only support X amount of tags"""
@@ -59,4 +33,4 @@ class LokiPublisher(object):
         tags = self._prepare_tags(message, fields_to_labels)
         self.logger.debug(f"TAGS[{len(tags)}]: {tags}")
         self.logger.debug(f"Logging entry:\n---\n{json.dumps(message)}\n---")
-        self.publisher.info(json.dumps(message), extra={'tags': tags})
+        self.emitter(json.dumps(message), extra={'tags': tags})
